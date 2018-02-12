0a63f7289eab45f218b38a4b7f6b82f2f15ea260
Henrik Enblom <henrik@enblom.com>
2018-02-12 21:41:26 +0100

    Added more dependencies

--- face-recognition-server.py
+++ face-recognition-server.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import face_recognition
-
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -9,15 +9,19 @@ app = Flask(__name__)
 def profile_image_upload():
     return 'Hello World!'
 
-# Load the jpg file into a numpy array
-image = face_recognition.load_image_file("biden.jpg")
 
-# Find all the faces in the image using a pre-trained convolutional neural network.
-# This method is more accurate than the default HOG model, but it's slower
-# unless you have an nvidia GPU and dlib compiled with CUDA extensions. But if you do,
-# this will use GPU acceleration and perform well.
-# See also: find_faces_in_picture.py
-face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model="cnn")
+def detect_faces_in_image(file_stream):
+    image = face_recognition.load_image_file(file_stream)
+
+    face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model="cnn")
+    print("I found {} face(s) in this photograph.".format(len(face_locations)))
+
+    for face_location in face_locations:
+        # Print the location of each face in this image
+        top, right, bottom, left = face_location
+        print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom,
+                                                                                                    right))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
