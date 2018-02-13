c2ef223d086383821420c0a80821b30034eac266
Henrik Enblom <henrik@enblom.com>
2018-02-13 22:55:03 +0100

    Cropping w PIL instead

--- face-recognition-server.py
+++ face-recognition-server.py
@@ -31,9 +31,7 @@ def detect_faces_in_image(file_stream, filename):
     face_image = image[top:bottom, left:right]
     pil_image = Image.fromarray(face_image)
 
-    print(left, top, right, bottom)
-
-    full_image.crop((left, top), (right, bottom))
+    full_image.crop((left, top, right, bottom))
 
     output_directory = "static/{}".format(filename)
 
